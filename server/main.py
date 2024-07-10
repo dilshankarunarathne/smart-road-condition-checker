8737dc878c22e792b3fbff85bc01b93fdc1ffcda
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-10 20:07:56 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes import auth
 
 app = FastAPI()
 
-app.include_router(auth.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
+app.include_router(auth.router)
