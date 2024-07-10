d1e4f7f5f6e9ebcc55f0a138ab03191cd1e7fc1c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-10 20:10:12 +0530

    Update locations.py

--- server/routes/locations.py
+++ server/routes/locations.py
@@ -13,52 +13,6 @@ router = APIRouter(
 )
 
 
-@router.post("/register")
-async def register_user(
-        username: str = Form(...),
-        email: str = Form(...),
-        password: str = Form(...),
-        is_admin=False
-):
-    if user_exists(username):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Username already exists",
-        )
-    hashed_password = get_password_hash(password)
-    user = UserInDB(
-        id=get_next_avail_id(),
-        username=username,
-        email=email,
-        hashed_password=hashed_password,
-        is_admin=is_admin,
-    )
-    add_new_user(user)
-    return user
-
-
-@router.post("/login")
-async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires.seconds
-        # Unresolved attribute reference 'username' for class 'bool'
-    )
-
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@router.post("/logout")
-async def logout(token: str = Depends(oauth2_scheme)):
-    blacklist_token(token)
-    return {"message": "Successfully logged out"}
+@router.post("/")
+async def get_locations():
+    return "Hello"
