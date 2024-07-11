2290b02a4742659de489f915b5e3d69cd566e7f7
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-11 23:13:50 +0530

    Update locations.py

--- server/routes/locations.py
+++ server/routes/locations.py
@@ -10,6 +10,17 @@ router = APIRouter(
 
 @router.get("/", response_class=HTMLResponse)
 async def get_locations():
+    coordinates = [
+        {"lat": "18.9750", "lng": "72.8258"},
+        {"lat": "19.0760", "lng": "72.8777"},
+        {"lat": "19.2183", "lng": "72.9781"}
+    ]
+
     with open('server/static/google map.html', 'r') as file:
         html_content = file.read()
+
+    coordinates_js = ', '.join([f'{{lat: "{coord["lat"]}", lng: "{coord["lng"]}"}}' for coord in coordinates])
+
+    html_content = html_content.replace('__COORDINATES__', coordinates_js)
+
     return html_content
