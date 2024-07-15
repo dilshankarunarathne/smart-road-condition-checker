032f0a54b36a92cb1e4a17e3ec318574db42cecb
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-15 11:50:37 +0530

    Update watcher.py

--- server/services/watcher.py
+++ server/services/watcher.py
@@ -15,5 +15,6 @@ def identified_pothole(number_of_pot_holes):
 
 def add_to_database(number_of_pot_holes, lon, lat):
     timestamp = datetime.now()
-    new_pothole = Pothole(_id="new_id", lon=123.456, lat=78.910, number_of_pot_holes=2, identified_time=timestamp)
+    new_pothole = Pothole(_id="new_id", lon=lon, lat=lat, number_of_pot_holes=number_of_pot_holes,
+                          identified_time=timestamp)
     new_pothole.save_to_mongo(db_name="your_db_name", collection_name="your_collection_name")
