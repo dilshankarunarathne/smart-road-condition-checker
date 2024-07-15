494e9c533cd75377bd7a5b25487743c5aa0349f3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-15 11:51:25 +0530

    Update video_stream.py

--- server/services/video_stream.py
+++ server/services/video_stream.py
@@ -16,7 +16,8 @@ cap = cv2.VideoCapture(URL + ":81/stream")
 def set_resolution(url: str, index: int = 1, verbose: bool = False):
     try:
         if verbose:
-            resolutions = "10: UXGA(1600x1200)\n9: SXGA(1280x1024)\n8: XGA(1024x768)\n7: SVGA(800x600)\n6: VGA(640x480)\n5: CIF(400x296)\n4: QVGA(320x240)\n3: HQVGA(240x176)\n0: QQVGA(160x120)"
+            resolutions = ("10: UXGA(1600x1200)\n9: SXGA(1280x1024)\n8: XGA(1024x768)\n7: SVGA(800x600)\n6: VGA("
+                           "640x480)\n5: CIF(400x296)\n4: QVGA(320x240)\n3: HQVGA(240x176)\n0: QQVGA(160x120)")
             print("available resolutions\n{}".format(resolutions))
 
         if index in [10, 9, 8, 7, 6, 5, 4, 3, 0]:
