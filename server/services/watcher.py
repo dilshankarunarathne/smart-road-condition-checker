e15a6c860e8a0bb04d98421a3c2f68b4daefe908
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-16 16:47:54 +0530

    Update watcher.py

--- server/services/watcher.py
+++ server/services/watcher.py
@@ -2,9 +2,25 @@ from datetime import datetime
 
 from server.models.pothole import Pothole
 
+import requests
+from bs4 import BeautifulSoup
+
 
 def get_location() -> (float, float):
-    pass  # TODO
+    ESP32_IP_ADDRESS = "http://192.168.1.100" 
+
+    # Make a GET request to the ESP32's IP address
+    response = requests.get(ESP32_IP_ADDRESS)
+
+    # Parse the HTML response using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extract the latitude and longitude from the HTML
+    # This assumes that the latitude and longitude are contained in elements with ids 'lat' and 'lon' respectively
+    lat = float(soup.find(id='lat').text)
+    lon = float(soup.find(id='lon').text)
+
+    return (lon, lat)
 
 
 def identified_pothole(number_of_pot_holes):
@@ -15,6 +31,6 @@ def identified_pothole(number_of_pot_holes):
 
 def add_to_database(number_of_pot_holes, lon, lat):
     timestamp = datetime.now()
-    new_pothole = Pothole(_id="new_id", lon=lon, lat=lat, number_of_pot_holes=number_of_pot_holes,
+    new_pothole = Pothole(lon=lon, lat=lat, number_of_pot_holes=number_of_pot_holes,
                           identified_time=timestamp)
-    new_pothole.save_to_mongo(db_name="your_db_name", collection_name="your_collection_name")
+    new_pothole.save_to_mongo(db_name="pothole", collection_name="your_collection_name")
