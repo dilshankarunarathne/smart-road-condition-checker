b3111a93a4c21c7f6f643e1a4fc805d4cfd76ea6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-16 17:13:43 +0530

    Update query.py

--- server/services/query.py
+++ server/services/query.py
@@ -1,3 +1,5 @@
+from server.models.pothole import Pothole
+
 all_locations = Pothole.get_all_locations(db_name="pothole", collection_name="your_collection_name")
 for location in all_locations:
     print(location)
